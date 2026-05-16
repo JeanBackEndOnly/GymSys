6ebd707edc3fae68585cfd2876b9ce52272e0d3a
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2026-05-16 18:25:38 +0800

    change 255 max lenght ot 190

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,12 +6,14 @@ use App\Models\User;
 use App\Policies\UserPolicy;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
         Gate::policy(User::class, UserPolicy::class);
+        Schema::defaultStringLength(190);
     }
     /**
      * Register any application services.
