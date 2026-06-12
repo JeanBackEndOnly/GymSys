dfba7e571e816372af74578a481cd5e77b490b7e
Jhon Harold Rueda <140807327+Ampexqt@users.noreply.github.com>
2026-06-12 14:56:57 +0800

    Update LandingPage.tsx

--- Frontend/src/pages/LandingPage.tsx
+++ Frontend/src/pages/LandingPage.tsx
@@ -5,15 +5,17 @@ import Features from '@/components/sections/Features';
 import Experience from '@/components/sections/Experience';
 import Membership from '@/components/sections/Membership';
 import MobileShowcase from '@/components/sections/MobileShowcase';
+import Location from '@/components/sections/Location';
 
 export default function LandingPage() {
   return (
     <div className="relative">
       <Navbar />
       <Hero />
+      <Location />
+      <Membership />
       <Features />
       <Experience />
-      <Membership />
       <MobileShowcase />
       <Footer />
     </div>
