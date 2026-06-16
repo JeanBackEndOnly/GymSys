62c123f91a3404d7306c91ee5b88a289a76f03ec
JeanBackEndOnly <145352326+JeanBackEndOnly@users.noreply.github.com>
2026-06-16 14:21:02 +0800

    make reservation

--- Backend/app/Http/Controllers/Admin/ReservationController.php
+++ Backend/app/Http/Controllers/Admin/ReservationController.php
@@ -11,6 +11,24 @@ use App\Http\Requests\Admin\ReservationRequest;
 class ReservationController extends Controller
 {
     use AuthorizesRequests;
+    
+    public function index(){
+        try {
+            $this->authorize('viewAny', Reservation::class);
+            $reservation = Reservation::all();
+            return response()->json([
+                'status' => 1,
+                'message' => 'reservation datas fetched successfully.',
+                'data' => $reservation,
+            ],200);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Failed to fetch trainers.',
+            ], 500);
+        }
+    }
+
     public function store(ReservationRequest $request){
         try {
             $validated = $request->validated();
@@ -22,11 +40,32 @@ class ReservationController extends Controller
                 'status' => 1,
                 'message' => 'Reservation created successfully.',
                 'data' => $reservations,
-            ]);
+            ],201);
         } catch (\Throwable $e) {
             return response()->json([
                 'status' => 0,
                 'message' => 'Failed to fetch trainers.',
+            ], 500);
+        }
+    }
+
+    public function update(ReservationRequest $request, $id){
+        try {
+            $reservation = Reservation::findOrFail($id);
+            $validated = $request->validated();
+            $this->authorize('update', $reservation);
+
+            $reservation->update($validated);
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'Reservation updated successfully.',
+                'data' => $reservation,
+            ],200);
+        } catch (\Throwable $e) {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Failed to updated reservation. ' . $e->getMessage(),
             ], 500);
         }
     }
